9235fbc7568d0bff0576da293e050bef383b94b9
appbr <107655534+appbr@users.noreply.github.com>
2022-09-15 14:06:37 -0300

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,44 @@
+var mario = window.document.getElementById('mario')
+var game = window.document.getElementById('game')
+var pipe = window.document.getElementById('pipe')
+
+game.addEventListener('keydown', pular)
+
+function pular(){
+    mario.classList.add('pular')
+
+    setTimeout(() => {
+        mario.classList.remove('pular')
+    }, 500);
+}
+
+
+
+var loop = setInterval(() => {
+        var pipePosition = pipe.offsetLeft;
+        var marioPosition = +window.getComputedStyle(mario).bottom.replace('px','');
+
+        
+        if (pipePosition <= 75 && pipePosition > 0 && marioPosition < 45) {
+            pipe.style.animation = 'none';
+            pipe.style.left = `${pipePosition}px`;
+
+            mario.style.animation = 'none';
+            mario.style.bottom = `${marioPosition}px`;
+
+            mario.src = 'images/game-over.png';
+            mario.style.width = '50px'
+            mario.style.marginLeft = '30px'
+
+
+            
+            clearInterval(loop);
+
+            // document.location.reload(true);
+
+            
+
+            
+        }
+
+    }, 10)    
