282e857c1a9699ec9ea8a96d2adb7ad8fd3cd03b
appbr <107655534+appbr@users.noreply.github.com>
2022-09-15 14:27:48 -0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -3,6 +3,7 @@ var game = window.document.getElementById('game')
 var pipe = window.document.getElementById('pipe')
 
 game.addEventListener('keydown', pular)
+game.addEventListener('touchstart', pular)
 
 function pular(){
     mario.classList.add('pular')
